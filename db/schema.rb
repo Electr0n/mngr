a20fb9654a6690208dbdf0f98a09d2467a6afe91
Electr0n <hunterlzp@yandex.ru>
2015-11-25 15:11:08 +0300

    Added some fields for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,24 +11,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151125072825) do
+ActiveRecord::Schema.define(version: 20151125112611) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "email",                  limit: 255, default: "",      null: false
+    t.string   "encrypted_password",     limit: 255, default: "",      null: false
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,       null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
     t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
+    t.datetime "created_at",                                           null: false
+    t.datetime "updated_at",                                           null: false
+    t.string   "name",                   limit: 255, default: "Vasya", null: false
+    t.string   "surname",                limit: 255
+    t.date     "bday"
+    t.string   "gender",                 limit: 255
+    t.integer  "age",                    limit: 4
+    t.integer  "phone",                  limit: 4
+    t.string   "country",                limit: 255
+    t.string   "city",                   limit: 255
+    t.string   "hobby",                  limit: 255
+    t.string   "about",                  limit: 255
+    t.string   "role",                   limit: 255, default: "user",  null: false
+    t.integer  "denied_t",               limit: 4
+    t.boolean  "locked",                             default: false,   null: false
   end
 
+  add_index "users", ["age"], name: "index_users_on_age", using: :btree
+  add_index "users", ["bday"], name: "index_users_on_bday", using: :btree
+  add_index "users", ["city"], name: "index_users_on_city", using: :btree
+  add_index "users", ["country"], name: "index_users_on_country", using: :btree
+  add_index "users", ["denied_t"], name: "index_users_on_denied_t", using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["gender"], name: "index_users_on_gender", using: :btree
+  add_index "users", ["hobby"], name: "index_users_on_hobby", using: :btree
+  add_index "users", ["locked"], name: "index_users_on_locked", using: :btree
+  add_index "users", ["name"], name: "index_users_on_name", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["role"], name: "index_users_on_role", using: :btree
+  add_index "users", ["surname"], name: "index_users_on_surname", using: :btree
 
 end
